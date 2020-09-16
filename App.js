db619353087d34e3ab61d69f7197aeaa6e6afbb2
PRATEEK PARDESHI <prateekpardeshi6@gmail.com>
2020-09-17 03:55:25 +0530

    updated expo^38.0.0 & improved code quality

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-
-import React,{Component} from 'react';
+import React, { Component } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -11,397 +10,245 @@ import {
   TouchableOpacity,
   Dimensions,
   Image,
-  TouchableHighlight
-  
-} from 'react-native';
+  TouchableHighlight,
+} from "react-native";
 
-import Header from './Header';
-import * as Google from 'expo-google-app-auth';
-import SecondScreen from './SecondScreen';
-import {KeyboardAvoidingView} from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import {AsyncStorage} from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import Scanner from './Scanner';
-import BlankScreen from './BlankScreen';
-import * as firebase from 'firebase';
+import Header from "./Header";
+import * as Google from "expo-google-app-auth";
+import SecondScreen from "./SecondScreen";
+import { KeyboardAvoidingView } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { AsyncStorage } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import Scanner from "./Scanner";
+import BlankScreen from "./BlankScreen";
+import * as firebase from "firebase";
 
-
-
-const userInfo = {username: 'admin', password: 'A'}
-
-
-
-// const App: () => React$Node = () => {
+const userInfo = { username: "admin", password: "A" };
 
 class LoginScreen extends Component {
-
   static navigationOptions = {
- 
-     header: null 
-  }
-    
+    header: null,
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state= {
-      username: '',
-      password: ''
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
 
-    }
-  }   
-
-
-
-
-
-
-render(){
-  return (
-    <>
-      {/* <StatusBar barStyle="dark-content" /> */}
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {/* {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )} */}
-          
-
+  render() {
+    return (
+      <>
+        {/* <StatusBar barStyle="dark-content" /> */}
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}
+          >
+            <Header />
           </ScrollView>
-      </SafeAreaView>
-           
-           
-     
+        </SafeAreaView>
 
-      <View style = {styles.viewStyleForLine}></View>
+        <View style={styles.viewStyleForLine}></View>
 
-
-  
-
-     
-     
-        <Image
-            style={styles.arrow}
-            source={require('./arrow.png')}
-        />
-       
+        <Image style={styles.arrow} source={require("./arrow.png")} />
 
         <KeyboardAwareScrollView
-      style={{ backgroundColor: '#fff' }}
-      resetScrollToCoords={{ x: 0, y: 0 }}
-      contentContainerStyle={styles.container}
-      scrollEnabled={false}
-    >   
-        <View style={styles.imcontainer}>
-        <Image
-            style={styles.logo}
-            source={require('./logo-unga.jpg')}
-        />
-        </View>
-
-
-        <Text style={styles.ur_text1}>USERNAME</Text> 
-
-
-
-        <View style={styles.ur_input1}>
-        <Image
-            style={styles.ur_img1}
-            source={require('./12.png')}
-        /> 
-      <TextInput 
-           
-           style={styles.input} 
-          //  placeholder="USERNAME"
-          onChangeText={(username)=>this.setState({username})}
-          value={this.state.username}
-          autoCapitalize="none"
-           
-      /> 
-      </View>
-
-
-       {/* <KeyboardAvoidingView style={{flex:1}} behavior="padding" enabled>  */}
-    
-     <Text style={styles.ur_text2}>PASSWORD</Text>
-
-
-   
-     {/* <KeyboardAvoidingView style={{flex:1}} behavior="padding" enabled>  */}
-
-     <View style={styles.ur_input2}>
-        <Image
-            style={styles.ur_img2}
-            source={require('./13.png')}
-        /> 
-          
-     <TextInput 
-           
-           style={styles.input2} 
-          //  placeholder="PASSWORD"
-           secureTextEntry
-           onChangeText={(password)=>this.setState({password})}
-           value={this.state.password}
-     />
-     </View>
-     {/* </KeyboardAvoidingView> */}
-     {/* </KeyboardAvoidingView> */}
-     
-           
-
-      <View >
-        <TouchableOpacity 
-            style= {styles.userBtn}
-            onPress= {this._login}
-        
-        
+          style={{ backgroundColor: "#fff" }}
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          contentContainerStyle={styles.container}
+          scrollEnabled={false}
         >
-          <Text style={styles.btnTxt}>LOGIN</Text>
-          
-        </TouchableOpacity>
-       </View>
+          <View style={styles.imcontainer}>
+            <Image style={styles.logo} source={require("./logo-unga.jpg")} />
+          </View>
 
-  <View style={styles.login_icons} >
+          <Text style={styles.ur_text1}>USERNAME</Text>
 
-  <TouchableHighlight onPress= {this._login_google}>
-        <Image
-            style={styles.icons_google}
-            source={require('./google.png')}
-        />
-        </TouchableHighlight>
-       
-        <Image
-            style={styles.icons_facebook}
-            source={require('./facebook.png')}
-        />
- </View>
+          <View style={styles.ur_input1}>
+            <Image style={styles.ur_img1} source={require("./12.png")} />
+            <TextInput
+              style={styles.input}
+              //  placeholder="USERNAME"
+              onChangeText={(username) => this.setState({ username })}
+              value={this.state.username}
+              autoCapitalize="none"
+            />
+          </View>
 
-      
+          <Text style={styles.ur_text2}>PASSWORD</Text>
 
+          <View style={styles.ur_input2}>
+            <Image style={styles.ur_img2} source={require("./13.png")} />
 
+            <TextInput
+              style={styles.input2}
+              //  placeholder="PASSWORD"
+              secureTextEntry
+              onChangeText={(password) => this.setState({ password })}
+              value={this.state.password}
+            />
+          </View>
 
-      <View style={styles.container_text}>
-        <Text style={styles.text}>FORGOT PASSWORD?</Text>
-        <Text style={styles.text}>REMEMBER ME</Text>
+          <View>
+            <TouchableOpacity style={styles.userBtn} onPress={this._login}>
+              <Text style={styles.btnTxt}>LOGIN</Text>
+            </TouchableOpacity>
+          </View>
 
+          <View style={styles.login_icons}>
+            <TouchableHighlight onPress={this._login_google}>
+              <Image
+                style={styles.icons_google}
+                source={require("./google.png")}
+              />
+            </TouchableHighlight>
 
-      </View>
+            <Image
+              style={styles.icons_facebook}
+              source={require("./facebook.png")}
+            />
+          </View>
 
+          <View style={styles.container_text}>
+            <Text style={styles.text}>FORGOT PASSWORD?</Text>
+            <Text style={styles.text}>REMEMBER ME</Text>
+          </View>
 
-      
-      <View style={styles.footer}>
-        <Text>Copyright 2019 UNGASoft Chile.All Right Reserved</Text>
-      </View>
+          <View style={styles.footer}>
+            <Text>Copyright 2019 UNGASoft Chile.All Right Reserved</Text>
+          </View>
+        </KeyboardAwareScrollView>
+      </>
+    );
+  }
 
-      </KeyboardAwareScrollView>
+  _login_google = async () => {
+    try {
+      const result = await Google.logInAsync({
+        //949582116880-0f19o6rkgf09e84tevjsljfc63h43igr.apps.googleusercontent.com
+        androidClientId:
+          "949582116880-mn2p527vqdo6cih7los79d3ocrg1hh6t.apps.googleusercontent.com",
 
-  {/* <View style={styles.footer}>
-     <Text>Copyright 2019 UNGASoft Chile.All Right Reserved</Text>
-   </View> */}
-  
- {/* </KeyboardAvoidingView>   */}
+        scopes: ["profile", "email"],
+      });
 
-
-
-
-   
-    </>
-);
-}
-
-_login_google = async() =>{
-  
-  try {
-    const result = await Google.logInAsync({
-
-
-      //949582116880-0f19o6rkgf09e84tevjsljfc63h43igr.apps.googleusercontent.com
-      androidClientId: "949582116880-mn2p527vqdo6cih7los79d3ocrg1hh6t.apps.googleusercontent.com",
-      //iosClientId: YOUR_CLIENT_ID_HERE,
-      scopes: ['profile', 'email'],
-    });
-
-    if (result.type === 'success') {
-
-
-      this.props.navigation.navigate('Second')
-
-     //return result.accessToken;
-    } else {
-      console.log("cancelled")
+      if (result.type === "success") {
+        this.props.navigation.navigate("Second");
+      } else {
+        console.log("cancelled");
+      }
+    } catch (e) {
+      console.log("error", e);
     }
-  } catch (e) {
-    console.log("error",e)
-  }
+  };
+
+  _login = async () => {
+    if (
+      userInfo.username === this.state.username &&
+      userInfo.password === this.state.password
+    ) {
+      this.props.navigation.navigate("Second");
+    } else {
+      alert("Username or Password is incorrect");
+    }
+  };
 }
-
-
-
-
-
-_login = async() =>{
-
-  if (userInfo.username === this.state.username && userInfo.password === this.state.password) {
-    // alert('Secured Logged In')
-    this.props.navigation.navigate('Second') 
-  } else {
-    alert('Username or Password is incorrect')
-  }
-
-}
-
-}
-
-
-
-// class DetailsScreen extends React.Component {
-
-
-
-//   static navigationOptions = {
- 
-//     header: null 
-//  }
-
-
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Details Screen</Text>
-//         <Button
-//           title="Go to Details... again"
-//           onPress={() => this.props.navigation.navigate('Login')}
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-
-
-
-
 
 const RootStack = createStackNavigator(
   {
     Login: LoginScreen,
     Second: SecondScreen,
     Scan: Scanner,
-    Details: BlankScreen
+    Details: BlankScreen,
   },
   {
-    initialRouteName: 'Login',
+    initialRouteName: "Login",
   }
 );
 
-
-
-const DEVICE_WIDTH = Dimensions.get('window').width;
-const DEVICE_HEIGHT = Dimensions.get('window').height;
+const DEVICE_WIDTH = Dimensions.get("window").width;
+const DEVICE_HEIGHT = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: '#000',
-    borderBottomColor: '#22abb6'
-  
+    backgroundColor: "#000",
+    borderBottomColor: "#22abb6",
   },
   engine: {
-    position: 'absolute',
+    position: "absolute",
     right: 0,
   },
   body: {
     backgroundColor: "#fff",
-    borderTopColor: '#22abb6'
+    borderTopColor: "#22abb6",
   },
 
   // BigContainer:{
   // height:1000,
-
 
   // },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
   },
-  // sectionTitle: {
-  //   fontSize: 24,
-  //   fontWeight: '600',
-  //   color: Colors.black,
-  // },
-  // sectionDescription: {
-  //   marginTop: 8,
-  //   fontSize: 18,
-  //   fontWeight: '400',
-  //   color: Colors.dark,
-  // },
+
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
-  // foroter: {
-  //   color: Colors.dark,
-  //   fontSize: 12,
-  //   fontWeight: '600',
-  //   padding: 4,
-  //   paddingRight: 12,
-  //   textAlign: 'right',
-  // },
 
   viewStyleForLine: {
-    borderBottomColor: '#22abb6', 
-    borderBottomWidth: 1, 
-    alignSelf:'stretch',
-    width: "100%"
+    borderBottomColor: "#22abb6",
+    borderBottomWidth: 1,
+    alignSelf: "stretch",
+    width: "100%",
   },
 
-  arrow:{
-   width:17,
-   height:13,
-   transform: [{ rotate: '180deg'}],
-   marginLeft: 25
-
+  arrow: {
+    width: 17,
+    height: 13,
+    transform: [{ rotate: "180deg" }],
+    marginLeft: 25,
   },
-
 
   imcontainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 40,
-    marginBottom: 20
+    marginBottom: 20,
   },
   logo: {
     width: 176,
     height: 66,
   },
-  ur_input1:{
-  flex: 1,
-  top: 0,
-  marginLeft:20,
-  width: DEVICE_WIDTH,
-  flexDirection: 'row',
-  // marginBottom: 20
+  ur_input1: {
+    flex: 1,
+    top: 0,
+    marginLeft: 20,
+    width: DEVICE_WIDTH,
+    flexDirection: "row",
+    // marginBottom: 20
   },
-  ur_input2:{
+  ur_input2: {
     // flex: 1,
     top: 20,
-    marginLeft:20,
+    marginLeft: 20,
     width: DEVICE_WIDTH,
-    flexDirection: 'row',
-    },
+    flexDirection: "row",
+  },
 
   ur_text1: {
     justifyContent: "center",
     textAlign: "center",
     marginTop: 40,
-    color: '#22abb6',
-    fontWeight: '900',
-    position: 'relative'
+    color: "#22abb6",
+    fontWeight: "900",
+    position: "relative",
   },
 
   ur_text2: {
@@ -409,56 +256,36 @@ const styles = StyleSheet.create({
     textAlign: "center",
     // marginTop: 10,
     top: 40,
-    color: '#22abb6',
-    fontWeight: '900',
+    color: "#22abb6",
+    fontWeight: "900",
     // position: 'relative'
   },
 
-
-  // ur_text2: {
-  //   justifyContent: "center",
-  //   textAlign: "center",
-  //   position: 'absolute',
-  //   marginTop: 0,
-  //   color: '#22abb6',
-  //   fontWeight: '900'
-  // },
-
-
-  ur_img1:{
-  
-  width:40,
-  height:30,
-  marginLeft:-5
-
-
+  ur_img1: {
+    width: 40,
+    height: 30,
+    marginLeft: -5,
   },
 
-  ur_img2:{
-  marginTop: 20,
-  width:35,
-  height:35
-  
-  
- },
-
+  ur_img2: {
+    marginTop: 20,
+    width: 35,
+    height: 35,
+  },
 
   input: {
-   width: "65%",
-   backgroundColor: "#fff",
-   height:36,
-   
-  //  paddingTop: 10,
-   marginLeft: 20,
-  //  marginBottom: 20,
-   marginTop: 0,
-   borderWidth: 2,
-  //  paddingBottom: 50,
-   
-   borderColor: 'transparent',
-   borderBottomColor: '#22abb6',
-  },
+    width: "65%",
+    backgroundColor: "#fff",
+    height: 36,
 
+    marginLeft: 20,
+
+    marginTop: 0,
+    borderWidth: 2,
+
+    borderColor: "transparent",
+    borderBottomColor: "#22abb6",
+  },
 
   input2: {
     width: "65%",
@@ -468,86 +295,76 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginTop: 20,
     borderWidth: 2,
-    
-    borderColor: 'transparent',
-    borderBottomColor: '#22abb6',
+
+    borderColor: "transparent",
+    borderBottomColor: "#22abb6",
   },
 
   userBtn: {
-  backgroundColor:'#22abb6',
-  paddingLeft: 25,
-  paddingRight: 15,
-  paddingBottom: 5,
-  paddingTop: 5,
-  marginTop:30,
-  width: "55%",
-  // marginTop:-30,
-  marginLeft: 80,
-  position: 'relative',
-  // textAlign: "center"
-  borderRadius: 100
+    backgroundColor: "#22abb6",
+    paddingLeft: 25,
+    paddingRight: 15,
+    paddingBottom: 5,
+    paddingTop: 5,
+    marginTop: 30,
+    width: "55%",
+    // marginTop:-30,
+    marginLeft: 80,
+    position: "relative",
+
+    borderRadius: 100,
   },
 
   btnTxt: {
-  
-  textAlign: "center",
-  paddingLeft: 0,
-  color: '#fff',
-  fontSize: 13
- },
+    textAlign: "center",
+    paddingLeft: 0,
+    color: "#fff",
+    fontSize: 13,
+  },
 
- login_icons: {
-  flex: 1,
-  top: 50,
-  width: DEVICE_WIDTH,
-  flexDirection: 'row',
-  justifyContent: 'space-evenly',
-},
+  login_icons: {
+    flex: 1,
+    top: 50,
+    width: DEVICE_WIDTH,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
 
-icons_google: {
-  width:50,
-  height:50,
-  // marginRight:35
-  
-},
-icons_facebook: {
-  width:50,
-  height:50,
-   marginLeft: -13
-  
-},
+  icons_google: {
+    width: 50,
+    height: 50,
+    // marginRight:35
+  },
+  icons_facebook: {
+    width: 50,
+    height: 50,
+    marginLeft: -13,
+  },
 
+  container_text: {
+    flex: 1,
+    top: 105,
+    width: DEVICE_WIDTH,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  text: {
+    color: "#22abb6",
+    backgroundColor: "transparent",
+    fontWeight: "800",
+  },
 
+  footer: {
+    color: "#000",
+    textAlign: "center",
+    marginLeft: 33,
+    paddingBottom: 18,
+    fontWeight: "600",
 
- container_text: {
-  flex: 1,
-  top: 105,
-  width: DEVICE_WIDTH,
-  flexDirection: 'row',
-  justifyContent: 'space-around',
-},
-text: {
-  color: '#22abb6',
-  backgroundColor: 'transparent',
-  fontWeight: '800'
-},
-
-footer: {
-
-  color: '#000',
-  textAlign: "center",
-  marginLeft: 33,
-  paddingBottom:18,
-  fontWeight: '600',
-  // alignItems: 'center',
-  // position: 'relative',
-  top: 10,
-  marginTop: 130,
-  bottom: 25
-
-},
-
-
+    top: 10,
+    marginTop: 130,
+    bottom: 25,
+  },
 });
 
 const AppContainer = createAppContainer(RootStack);
--- Scanner.js
+++ Scanner.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, View, Text, Modal, Button, TouchableOpacity, Alert } from 'react-native';
- 
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Modal,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default class BarcodeScannerExample extends React.Component {
-
-
   constructor(props) {
- 
     super(props);
- 
-    this.state = { 
-        
-        Alert_Visibility: false 
+
+    this.state = {
+      Alert_Visibility: false,
     };
- 
   }
- 
+
   Show_Custom_Alert(visible) {
- 
-    this.setState({Alert_Visibility: visible});
-    
+    this.setState({ Alert_Visibility: visible });
   }
- 
-  ok_Button=()=>{
 
-
+  ok_Button = () => {
     // Alert.alert(this.state.bar);
- 
-    this.props.navigation.navigate('Details', {
+
+    this.props.navigation.navigate("Details", {
       JSON_ListView_Clicked_Item: this.state.bar,
-    })
-    this.Show_Custom_Alert(!this.state.Alert_Visibility)
- 
-  }
+    });
+    this.Show_Custom_Alert(!this.state.Alert_Visibility);
+  };
 
   state = {
     hasCameraPermission: null,
@@ -49,8 +48,8 @@ export default class BarcodeScannerExample extends React.Component {
 
   getPermissionsAsync = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
+    this.setState({ hasCameraPermission: status === "granted" });
+  };
 
   render() {
     const { hasCameraPermission, scanned } = this.state;
@@ -65,9 +64,10 @@ export default class BarcodeScannerExample extends React.Component {
       <View
         style={{
           flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-        }}>
+          flexDirection: "column",
+          justifyContent: "flex-end",
+        }}
+      >
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
@@ -75,163 +75,124 @@ export default class BarcodeScannerExample extends React.Component {
 
         {scanned && (
           <Button
-            title={'Tap to Scan Again'}
+            title={"Tap to Scan Again"}
             onPress={() => this.setState({ scanned: false })}
           />
         )}
 
-
-
-
-      {/* </View>
+        {/* </View>
       <View style={styles.MainContainer}> */}
- 
- <Modal
 
-   visible={this.state.Alert_Visibility}
+        <Modal
+          visible={this.state.Alert_Visibility}
+          transparent={false}
+          animationType={"fade"}
+          onRequestClose={() => {
+            this.Show_Custom_Alert(!this.state.Alert_Visibility);
+          }}
+        >
+          <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <View style={styles.Alert_Main_View}>
+              <Text style={styles.Alert_Title}>Item has been Scanned!</Text>
 
-   transparent={false}
+              <View
+                style={{ width: "100%", height: 2, backgroundColor: "#fff" }}
+              />
 
-   animationType={"fade"}
+              <Text style={styles.Alert_Message}> Code: {this.state.bar} </Text>
 
-   onRequestClose={ () => { this.Show_Custom_Alert(!this.state.Alert_Visibility)} } >
+              <View
+                style={{ width: "100%", height: 1, backgroundColor: "#fff" }}
+              />
 
+              <View style={{ flexDirection: "row", height: "30%" }}>
+                <TouchableOpacity
+                  style={styles.buttonStyle}
+                  onPress={this.ok_Button}
+                  activeOpacity={0.7}
+                >
+                  <Text style={styles.TextStyle}> OK </Text>
+                </TouchableOpacity>
 
-     <View style={{ flex:1, alignItems: 'center', justifyContent: 'center' }}>
+                <View
+                  style={{ width: 1, height: "100%", backgroundColor: "#fff" }}
+                />
 
-
-         <View style={styles.Alert_Main_View}>
-
-
-             <Text style={styles.Alert_Title}>Item has been Scanned!</Text>
-
-
-             <View style={{ width: '100%', height: 2, backgroundColor: '#fff'}} />
-
-
-             <Text style={styles.Alert_Message}> Code: {this.state.bar}  </Text>
-
-
-             <View style={{ width: '100%', height: 1, backgroundColor: '#fff'}} />
-
-
-             <View style={{flexDirection: 'row', height: '30%'}}>
-
-                 <TouchableOpacity 
-                     style={styles.buttonStyle}
-                     onPress={this.ok_Button} 
-                     activeOpacity={0.7} 
-                      >
-
-                     <Text style={styles.TextStyle}> OK </Text>
-         
-                 </TouchableOpacity>
-
-                 <View style={{ width: 1, height: '100%', backgroundColor: '#fff'}} />
-
-                 <TouchableOpacity 
-                     style={styles.buttonStyle} 
-                     onPress={() => { this.Show_Custom_Alert(!this.state.Alert_Visibility)} } 
-                     activeOpacity={0.7} 
-                     >
-
-                     <Text style={styles.TextStyle}> CANCEL </Text>
-         
-                 </TouchableOpacity>
-
-             </View>
-           
-         </View>
-
-     </View>
-
-
- </Modal>
-
- {/* <Button onPress={() => {  }} title="Click Here To Show Custom Alert Dialog" /> */}
-  
-
-</View>
-
-
-
-
-
-
+                <TouchableOpacity
+                  style={styles.buttonStyle}
+                  onPress={() => {
+                    this.Show_Custom_Alert(!this.state.Alert_Visibility);
+                  }}
+                  activeOpacity={0.7}
+                >
+                  <Text style={styles.TextStyle}> CANCEL </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
     );
   }
 
   handleBarCodeScanned = ({ type, data }) => {
     this.setState({ scanned: true });
-    // alert(this.state.bar);
 
-    this.Show_Custom_Alert(true)
+    this.Show_Custom_Alert(true);
 
     const bar_code = data;
-   // let bar = Object.values(bar_code);
-    this.setState({ bar: bar_code});
+    this.setState({ bar: bar_code });
   };
 }
 
-
 const styles = StyleSheet.create({
- 
-  MainContainer :{
-      
-   flex:1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   marginTop: (Platform.OS == 'ios') ? 20 : 0
-   
+  MainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: Platform.OS == "ios" ? 20 : 0,
   },
-   
-  Alert_Main_View:{
-   
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor : "#009688", 
-    height: 200 ,
-    width: '90%',
+
+  Alert_Main_View: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#009688",
+    height: 200,
+    width: "90%",
     borderWidth: 1,
-    borderColor: '#fff',
-    borderRadius:7,
-   
+    borderColor: "#fff",
+    borderRadius: 7,
   },
-   
-  Alert_Title:{
-   
-    fontSize: 25, 
+
+  Alert_Title: {
+    fontSize: 25,
     color: "#fff",
-    textAlign: 'center',
+    textAlign: "center",
     padding: 10,
-    height: '28%'
-   
+    height: "28%",
   },
-   
-  Alert_Message:{
-   
-      fontSize: 22, 
-      color: "#fff",
-      textAlign: 'center',
-      padding: 10,
-      height: '42%'
-     
-    },
-   
+
+  Alert_Message: {
+    fontSize: 22,
+    color: "#fff",
+    textAlign: "center",
+    padding: 10,
+    height: "42%",
+  },
+
   buttonStyle: {
-      
-      width: '50%',
-      height: '100%',
-      justifyContent: 'center',
-      alignItems: 'center'
-   
+    width: "50%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
-     
-  TextStyle:{
-      color:'#fff',
-      textAlign:'center',
-      fontSize: 22,
-      marginTop: -5
-  }
-   
-  });
+
+  TextStyle: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 22,
+    marginTop: -5,
+  },
+});
